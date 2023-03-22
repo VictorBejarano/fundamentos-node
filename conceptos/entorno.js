eb852108232f57794f04599465aed35134e67915
Victor Bejarano <victor.bejarano@sophossolutions.com>
2023-03-22 13:16:39 -0500

    conceptos

--- conceptos/entorno.js
+++ conceptos/entorno.js
@@ -2,4 +2,5 @@ let nombre = process.env.NOMBRE || "Sin nombre";
 let web = process.env.WEB || "No tengo web";
 
 console.log("Hola " + nombre);
-console.log("Mi web es " + web);
+console.log("Mi web esta en " + web);
+//Quede en Herramientas para ser mas feliz
